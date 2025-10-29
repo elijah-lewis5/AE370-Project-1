128143d014f6d64199cbf224d726900933f075cd
elijah-lewis5 <eqlewis2@illinois.edu>
2025-10-29 14:38:30 -0500

    Update initial_conditions.py

--- data/initial_conditions.py
+++ data/initial_conditions.py
@@ -9,5 +9,5 @@ m2 = 900  #kg https://www.issfd.org/ISSFD_2012/ISSFD23_IN1_1.pdf
 l  = 7.62 #m  https://www.smithsonianmag.com/air-space-magazine/how-things-work-dropping-in-on-mars-135812/
 k  = 2.0e3#N/m
 b  = 3.2e3#kg/s
-g  = 3.71 #m/s^2 
+g  = 3.728#m/s^2 
 constants = [m1, m2, l, k, b, g]
