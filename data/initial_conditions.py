cc170a4fcbe78415ee361d4cdbd724e28e9aedfc
elijah-lewis5 <eqlewis2@illinois.edu>
2025-10-29 14:33:14 -0500

    Update initial_conditions.py
    made the conditions more physical based

--- data/initial_conditions.py
+++ data/initial_conditions.py
@@ -7,7 +7,7 @@ Defines initial conditions for the skycrane.
 m1 = 1070 #kg https://www.issfd.org/ISSFD_2012/ISSFD23_IN1_1.pdf
 m2 = 900  #kg https://www.issfd.org/ISSFD_2012/ISSFD23_IN1_1.pdf
 l  = 7.62 #m  https://www.smithsonianmag.com/air-space-magazine/how-things-work-dropping-in-on-mars-135812/
-k  = 1000 #N/m
-b  = 1000 #kg/s
-g  = 3.73 #m/s^2 
+k  = 2.0e3#N/m
+b  = 3.2e3#kg/s
+g  = 3.71 #m/s^2 
 constants = [m1, m2, l, k, b, g]
