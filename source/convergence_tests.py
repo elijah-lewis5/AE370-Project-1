321451ccab4d62db9269a582a9c43fa0c8beacf1
jrees6 <jmrees2@illinois.edu>
2025-10-30 19:00:57 -0500

    Update Jackson error convergence

--- source/convergence_tests.py
+++ source/convergence_tests.py
@@ -4,7 +4,7 @@ from source.eoms import *
 def ivp_ab3_error(u_0, T, delta_t, delta_t_baseline):
     
     """Final-time relative error: ||u(T)_dt - u(T)_baseline|| / ||u(T)_baseline||."""
-    u_dt, _   = ab3_solve(skycrane_damping, u_0, delta_t, T)
-    u_base, _ = ab3_solve(skycrane_damping, u_0, delta_t_baseline, T)
-    uT_dt, uT_base = u_dt[-1], u_base[-1]
-    return np.linalg.norm(uT_dt - uT_base) / max(np.linalg.norm(uT_base), 1e-16)
+    u_dt, _   = ab3_solve(skycrane_damping, u_0, delta_t, T) #Broader time step less accurate results
+    u_base, _ = ab3_solve(skycrane_damping, u_0, delta_t_baseline, T) #Finer time step more accurate results
+    uT_dt, uT_base = u_dt[-1], u_base[-1] #final state at time T for both simulations
+    return np.linalg.norm(uT_dt - uT_base) / max(np.linalg.norm(uT_base), 1e-16) #relative error calculation between both results then divides by the norm
---
+++ source/convergence_tests_undamp.py
@@ -0,0 +1,10 @@
+from source.numerical_methods import *
+import numpy as np
+from source.eoms import *
+def ivp_ab3_errorundamped(u_0, T, delta_t, delta_t_baseline):
+    
+    """Final-time relative error: ||u(T)_dt - u(T)_baseline|| / ||u(T)_baseline||."""
+    u_dt, _   = ab3_solve(skycrane, u_0, delta_t, T)
+    u_base, _ = ab3_solve(skycrane, u_0, delta_t_baseline, T)
+    uT_dt, uT_base = u_dt[-1], u_base[-1]
+    return np.linalg.norm(uT_dt - uT_base) / max(np.linalg.norm(uT_base), 1e-16)
